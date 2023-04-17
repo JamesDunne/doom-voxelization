9decdec2880d39bbad8b68e03db70963d2752947
jsd1982 <james.jdunne@gmail.com>
2023-04-17 11:11:33 -0500

    Process 8 rotation frames

--- main.go
+++ main.go
@@ -47,10 +47,37 @@ func main() {
 		})
 	}
 
-	{
-		lump := wc.FindLumpBetween("TROOA2C8", "S_START", "S_END")
+	// find image extents of full rotation:
+	maxwidth := 0
+	maxheight := 0
+	for _, name := range []string{"TROOA1", "TROOA2C8", "TROOA3C7", "TROOA4C6", "TROOA5", "TROOC4A6", "TROOC3A7", "TROOC2A8"} {
+		lump := wc.FindLumpBetween(name, "S_START", "S_END")
 		if lump == nil {
-			panic("could not find lump")
+			panic("could not find lump " + name)
+		}
+		spr := lump.Data
+
+		width := int(le.Uint16(spr[0:2]))
+		height := int(le.Uint16(spr[2:4]))
+		leftoffs := int(int16(le.Uint16(spr[4:6])))
+		topoffs := int(int16(le.Uint16(spr[6:8])))
+
+		width += leftoffs
+		height += 16
+		if width > maxwidth {
+			maxwidth = width
+		}
+		if height > maxheight {
+			maxheight = height
+		}
+		// TODO leftoffs, topoffs
+		_, _ = leftoffs, topoffs
+	}
+
+	for p, name := range []string{"TROOA1", "TROOA2C8", "TROOA3C7", "TROOA4C6", "TROOA5", "TROOC4A6", "TROOC3A7", "TROOC2A8"} {
+		lump := wc.FindLumpBetween(name, "S_START", "S_END")
+		if lump == nil {
+			panic("could not find lump " + name)
 		}
 
 		spr := lump.Data
@@ -59,11 +86,16 @@ func main() {
 		{
 			width := int(le.Uint16(spr[0:2]))
 			height := int(le.Uint16(spr[2:4]))
-			leftoffs := int16(le.Uint16(spr[4:6]))
-			topoffs := int16(le.Uint16(spr[6:8]))
-			_, _ = leftoffs, topoffs
+			leftoffs := int(int16(le.Uint16(spr[4:6])))
+			topoffs := int(int16(le.Uint16(spr[6:8])))
+			_, _, _ = leftoffs, topoffs, height
 
-			img := image.NewPaletted(image.Rect(0, 0, width, height), pal)
+			fmt.Printf("%-8s: %d, %d\n", name, leftoffs, topoffs)
+
+			img := image.NewPaletted(image.Rect(0, 0, maxwidth, maxheight), pal)
+
+			xadj := maxwidth/2 - leftoffs
+			yadj := maxheight - 16 - topoffs
 
 			for i := 0; i < width; i++ {
 				runOffs := le.Uint32(spr[8+i*4 : 8+i*4+4])
@@ -77,38 +109,38 @@ func main() {
 						break posts
 					}
 
-					//fmt.Printf("\033[%dG", ystart*3+1)
-					//for j := 0; j < ystart; j++ {
-					//	fmt.Printf("   ")
-					//}
-
 					length := int(run[1])
 					// skip unused byte
 					runOffs += 3
-					for j := 3; j < 3+length; j++ {
-						img.SetColorIndex(i, ystart+j-3, run[j])
-						//fmt.Printf("%02x ", run[j])
+					if p >= 5 {
+						// h-flip:
+						for j := 3; j < 3+length; j++ {
+							img.SetColorIndex(xadj+width-1-i, yadj+ystart+j-3, run[j])
+						}
+					} else {
+						for j := 3; j < 3+length; j++ {
+							img.SetColorIndex(xadj+i, yadj+ystart+j-3, run[j])
+						}
 					}
 					runOffs += uint32(length)
 					runOffs++
 				}
-				//fmt.Print("\n")
 			}
 
-			for y := 0; y < height; y++ {
-				for x := 0; x < width; x++ {
-					c := img.ColorIndexAt(x, y)
-					if c != 0 {
-						fmt.Printf("%02x ", c)
-					} else {
-						fmt.Print("   ")
-					}
-				}
-				fmt.Print("\n")
-			}
+			//for y := 0; y < height; y++ {
+			//	for x := 0; x < width; x++ {
+			//		c := img.ColorIndexAt(x, y)
+			//		if c != 0 {
+			//			fmt.Printf("%02x ", c)
+			//		} else {
+			//			fmt.Print("   ")
+			//		}
+			//	}
+			//	fmt.Print("\n")
+			//}
 
 			func() {
-				f, err := os.OpenFile("troo.png", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+				f, err := os.OpenFile(fmt.Sprintf("TROOA%d.png", p+1), os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
 				if err != nil {
 					panic(err)
 				}
